c61b32b2e27c36488ca0a4dd34fb38b344162842
MadhavMahajan <madhav.mhjn01@gmail.com>
2024-07-01 03:33:15 +0530

    Update customreact.js

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -1,8 +1,11 @@
 function customrender(reactelement, container) {
   let domelement = document.createElement(reactelement.type);
   domelement.innerHTML = reactelement.children;
-  domelement.setAttribute("href", reactelement.props.href);
-  domelement.setAttribute('target', reactelement.props.target);
+  // domelement.setAttribute("href", reactelement.props.href);
+  // domelement.setAttribute('target', reactelement.props.target);
+  for (const prop in reactelement.props) {
+    domelement.setAttribute(prop,reactelement.props[prop]);
+  }
   container.appendChild(domelement);
 }
 let reactelement = {
