c75ddce1c69726055bac92693cb9c24daa8a1d0c
MadhavMahajan <madhav.mhjn01@gmail.com>
2024-07-02 14:17:23 +0530

    props and it application

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,10 +6,18 @@ import Card from "./components/card";
 function App() {
   // const [count, setCount] = useState(0)
 
+  let myobj={
+    username: 'madhav1010',
+    password: 'letmetwist',
+    age:10,
+  }
+  let myarr=[1,2,3,4,5];
+
   return (
     <>
       <h1 className="bg-green-600 text-black p-5 rounded-xl">Tailwind Test</h1>
-      <Card />
+      <Card channel='chai aur code' obj={myobj} myarr={myarr} buttontext='view me'/>
+      <Card channel='madhav mahajan'></Card>
     </>
   );
 }
