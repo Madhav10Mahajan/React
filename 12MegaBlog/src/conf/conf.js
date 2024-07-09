7690137bfc15347951a6c1594c80b6a80ae82cd3
MadhavMahajan <madhav.mhjn01@gmail.com>
2024-07-10 03:15:41 +0530

    Mega Blog

---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,8 @@
+let conf = {
+  appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+  appwriteBucketID: String(import.meta.env.VITE_BUCKET-ID),
+  appwriteCollectioId: String(import.meta.env.VITE_COLLECTION_ID),
+};
+export default conf;
